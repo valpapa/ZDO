361ae973529f45cec7db9ab99d1c7886734d466e
valpapa <135045411+valpapa@users.noreply.github.com>
2023-06-01 17:12:25 +0200

    Update run.py

--- src/run.py
+++ src/run.py
@@ -27,12 +27,18 @@ import json
 import argparse
 
 
-def prog(name_out,  *pics):    #visual_mode=False,
-    text_color=None
+def prog(name_out,visual_mode=False,  *pics):    #visual_mode=False,
     data=[]
+    obr1=[]
+    maxp1=[]
+    maxl1=[]
+    bod11=[]
+    bod21=[]
+    pojistka=[]
     for path in pics:
         #plt.figure()
         text_color = skimage.io.imread(path)
+        obr1.append(text_color)
         #plt.imshow(text_color)
         text_gray = skimage.color.rgb2gray(text_color)
         textb = text_gray > 0.35
@@ -45,21 +51,23 @@ def prog(name_out,  *pics):    #visual_mode=False,
 
 
 
-        size=len(text_color[1])//100
+        size=len(text_color[1])//120
+        #print(size)
         kernel_big = skimage.morphology.diamond(size)
         kernel = np.ones((5, 1), dtype=np.uint8)
 
         textd = skimage.morphology.binary_closing(textb, kernel_big)  #erosion dilation opening closing
 #plt.figure()
 #plt.imshow(textd, cmap='gray')
-
-        kernel = np.ones((5, 1), dtype=np.uint8)
+        #plt.figure()
+        #plt.imshow(textd)
+        kernel = np.ones((1, 5), dtype=np.uint8)
 #kernel=skimage.morphology.diamond(3)
-        textd = skimage.morphology.binary_erosion(textd, kernel)
+        #textd = skimage.morphology.binary_dilation(textd, kernel)
 
 
-#plt.figure()
-#plt.imshow(textd, cmap='gray')
+        #plt.figure()
+        #plt.imshow(textd)
 #print(kernel)
 
 
@@ -69,8 +77,8 @@ def prog(name_out,  *pics):    #visual_mode=False,
 
 
         image=skela
-#plt.figure()
-#plt.imshow(image)
+        #plt.figure()
+        #plt.imshow(image)
 #binary_image=np.logical_not(image)
         binary_image=image
         #plt.figure()
@@ -99,7 +107,12 @@ def prog(name_out,  *pics):    #visual_mode=False,
  #   plt.plot(X,Y)
 
         p=0
-        x=0              
+        x=0
+        
+        if len(points)==0:
+            del obr1[-1]            
+            continue
+            
         while True:
             if   len(text_color)-len(text_color)//8 < points[x][1] or points[x][1]< len(text_color)//8 or len(text_color[1])-len(text_color[1])//12 <points[x][0] or points[x][0]< len(text_color[1])//12:
                 points.remove(points[x]) 
@@ -110,7 +123,7 @@ def prog(name_out,  *pics):    #visual_mode=False,
                     break
                 p=1   
                 x=0   
-
+        
 
 
         
@@ -118,13 +131,17 @@ def prog(name_out,  *pics):    #visual_mode=False,
         maxp=(0,0)
         bod1=[]
         bod2=[]
-
+        
+        stred=4
         for point in points:
-            if maxl[0]>point[0] and len(text_color)/2+len(text_color)//8 >point[1]> len(text_color)/2-len(text_color)//8:
+            if maxl[0]>point[0] and len(text_color)/2+len(text_color)//stred >point[1]> len(text_color)/2-len(text_color)//stred:
                 maxl=point
-            if maxp[0]<point[0] and len(text_color)/2+len(text_color)//8>point[1]> len(text_color)/2-len(text_color)//8:
+            if maxp[0]<point[0] and len(text_color)/2+len(text_color)//stred>point[1]> len(text_color)/2-len(text_color)//stred:
                 maxp=point
-
+			
+        
+        maxp1.append(maxp)
+        maxl1.append(maxl)
         #plt.figure()
         #plt.plot([maxp[0],maxl[0]],[maxp[1],maxl[1]], linestyle='-', color='blue')
         #plt.xlim(0, len(binary_image[0]))
@@ -144,12 +161,20 @@ def prog(name_out,  *pics):    #visual_mode=False,
                 points=points[:]
                 rozptyl=rozptyl+5
             max_value = max(points, key=lambda x: x[1])
+            
             if abs(max_value[1]-maxl[1])<rozptyl and abs(maxl[0]-max_value[0])<abs(maxp[0]-max_value[0]):
                 points.remove(max_value)
+                if len(points)==0:
+                    pomocny=1
+                    break
                 max_value = max(points, key=lambda x: x[1])
                 continue
+                
+     
+                
         
-            if abs(max_value[1]-maxp[1])<rozptyl and abs(maxl[0]-max_value[0])>abs(maxp[0]-max_value[0]):
+            if abs(max_value[1]-maxp[1])<rozptyl and  abs(maxl[0]-max_value[0])>abs(maxp[0]-max_value[0]):
+         
                 break    
     
             min_value=(1000,1000)
@@ -177,7 +202,8 @@ def prog(name_out,  *pics):    #visual_mode=False,
                 bod1+=[max_value]
                 bod2+=[min_value]
   
-
+        bod11.append(bod1)
+        bod21.append(bod2)
         dist=[]
         angles=[]
         #plt.imshow(text_color)
@@ -205,7 +231,7 @@ def prog(name_out,  *pics):    #visual_mode=False,
         with open(name_out, 'w', encoding='utf-8') as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
             
-            
+        '''   
         # vykresleni vysledku    
         plt.figure()
         plt.imshow(text_color)
@@ -216,21 +242,22 @@ def prog(name_out,  *pics):    #visual_mode=False,
         plt.imshow(text_color)
         for x in range(0,len(bod1)):
             plt.plot([bod1[x][0],bod2[x][0]],[bod1[x][1],bod2[x][1]], linestyle='-', color='red')
-    '''    
+        '''    
     if visual_mode :
-        
-        plt.figure()
-        plt.imshow(text_color)
-        plt.figure()
-        plt.plot([maxp[0],maxl[0]],[maxp[1],maxl[1]], linestyle='-', color='blue')
-        plt.xlim(0, len(binary_image[0]))
-        plt.ylim( len(binary_image),0)
-        plt.imshow(text_color)
-        for x in range(0,len(bod1)):
-            plt.plot([bod1[x][0],bod2[x][0]],[bod1[x][1],bod2[x][1]], linestyle='-', color='red')
+        for x in range(0,len(obr1)):            
+            plt.figure()
+            plt.imshow(obr1[x])
+            plt.figure()
+            plt.plot([maxp1[x][0],maxl1[x][0]],[maxp1[x][1],maxl1[x][1]], linestyle='-', color='blue')
+        #plt.xlim(0, len(obr1[x][0]))
+        #plt.ylim( len(obr1[x]),0)
+            plt.imshow(obr1[x])
+            for i in range(0,len(bod11[x])):
+                #print(len(bod11[x][i]))
+                plt.plot([bod11[x][i][0],bod21[x][i][0]],[bod11[x][i][1],bod21[x][i][1]], linestyle='-', color='red')
          
-        print('ok')
-    '''
+        plt.show()
+    
             
 
 
@@ -241,4 +268,4 @@ if __name__ == "__main__":
     parser.add_argument("image_files", nargs="+", help="Input image files")
     args = parser.parse_args()
 
-    prog(args.output_file,  *args.image_files)  #args.visual_mode,
+    prog(args.output_file, args.visual_mode, *args.image_files)  #args.visual_mode,
